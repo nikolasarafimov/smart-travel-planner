747cb4b26d783d124a632dd9ca8373b8eff5fddd
Nikola Sarafimov <sarafimov.nikola12345@gmail.com>
2026-06-30 15:24:16 +0200

    Created mcp module

---
+++ mcp-server/src/test/java/com/smarttravel/mcp/McpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttravel.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
