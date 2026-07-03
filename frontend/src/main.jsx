e499582e90c8df302a3571ae9b6d6f7ce4c85cbb
Nikola Sarafimov <sarafimov.nikola12345@gmail.com>
2026-07-03 17:53:45 +0200

    Created frontend structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import keycloak from './auth/keycloak.js'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+keycloak
+    .init({
+        onLoad: 'check-sso',
+        pkceMethod: 'S256',
+        checkLoginIframe: false
+    })
+    .then(() => {
+        ReactDOM.createRoot(document.getElementById('root')).render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        )
+    })
+    .catch((error) => {
+        console.error('Keycloak initialization failed', error)
+    })
